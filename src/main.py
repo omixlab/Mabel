6f9705796894d368150e8f0f69cc312a8ac5e18e
Gabriel Liston <gabriellistondemenek@gmail.com>
2023-05-18 03:23:31 -0300

    streamlit_v3
    mudança no layout, ainda falta os termos de busca

--- src/main.py
+++ src/main.py
@@ -21,27 +21,39 @@ def main():
 
     #        st.write("Welcome, ", email)
 
+# INTERFACE DE BUSCA
+    keyword = st.text_input("Keyword")
 
-    keyword = st.sidebar.text_input("Keyword")
+    st.write('Select your desired databases')
+    col1, col2 = st.columns([1,2])
+    with col1:
+        pubmed_check = st.checkbox('PubMed', True)
+        
+    with col2:
+        articles_range = [1, 5, 10, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
+        num_pubmed = st.select_slider('Number of PubMed articles: ', options=articles_range, value=5000, disabled=(not pubmed_check))
 
-    st.sidebar.write('Select your desired databases')
-    articles_range = [1, 5, 10, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
-    
-    pubmed_check = st.sidebar.checkbox('PubMed', True)
-    num_pubmed = st.sidebar.select_slider('Number of PubMed articles: ', options=articles_range, value=5000, disabled=(not pubmed_check))
+    col1, col2 = st.columns([1,2])
+    with col1:
+        scopus_check = st.checkbox('Scopus', True)
+    with col2:
+        num_scopus = st.select_slider('Number of Scopus articles: ', options=[25, 5000], value=5000, disabled=(not scopus_check))
 
-    scopus_check = st.sidebar.checkbox('Scopus', True)
-    num_scopus = st.sidebar.select_slider('Number of Scopus articles: ', options=[25, 5000], value=5000, disabled=(not scopus_check))
-
-    scidir_check = st.sidebar.checkbox('ScienceDirect', True)
-    num_scidir = st.sidebar.select_slider('Number of ScienceDirect articles: ', options=[25, 5000], value=5000, disabled=(not scidir_check))
+    col1, col2 = st.columns([1,2])
+    with col1:
+        scidir_check = st.checkbox('ScienceDirect', True)
+    with col2:
+        num_scidir = st.select_slider('Number of ScienceDirect articles: ', options=[25, 5000], value=5000, disabled=(not scidir_check))
 
     @st.cache_data
     def convert_df(df):
         return df.to_csv().encode("utf-8")
 
-    if st.sidebar.button("Submit"):
-        st.write("""## Extracting, please wait""")
+
+# RESULTADOS
+    if st.button("Submit"):
+        st.sidebar.write("""## Extracting...""")
+
         # BUSCA PARA O PUBMED
         if pubmed_check:
             with st.spinner(f'Searching articles with keyword "{keyword}" in PubMed ({num_pubmed}) wait...'):
@@ -49,7 +61,7 @@ def main():
 
                 data = convert_df(data_tmp)
 
-                st.download_button(
+                st.sidebar.download_button(
                     label="Download PubMed data as CSV",
                     data=data,
                     file_name=f"pubmed_df.csv",
@@ -81,7 +93,7 @@ def main():
 
                 data = convert_df(data_tmp)
 
-                st.download_button(
+                st.sidebar.download_button(
                     label="Download ScienceDirect data as CSV",
                     data=data,
                     file_name=f"scidir_df.csv",
@@ -89,10 +101,7 @@ def main():
                 )
 
                 st.success("ScienceDirect Done!")
-
-    else:
-        st.write("""## Insert your keyword & choose database""")
-
+        st.sidebar.success("Extraction complete!")
 
 
 if __name__ == "__main__":
