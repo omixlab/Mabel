9e93916452505450d6f78cccd0a5e945bbba410a
Gabriel Liston <gabriellistondemenek@gmail.com>
2023-05-19 15:27:08 -0300

    Alteração na estrutura (WIP)

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import time
 import streamlit as st
 
 from extractor import Extractor
+from search_input import basic, advanced
 
 
 def main():
@@ -22,70 +23,24 @@ def main():
 
     # BASIC INPUT
     if mode == "Basic":
-        keyword = st.text_input("Keyword")
-
-        st.write('Select your desired databases')
-        col1, col2 = st.columns([1,2])
-        with col1:
-            pubmed_check = st.checkbox('PubMed', True)
-        with col2:
-            articles_range = [1, 5, 10, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
-            num_pubmed = st.select_slider('Number of PubMed articles: ', options=articles_range, value=5000, disabled=(not pubmed_check))
-
-        col1, col2 = st.columns([1,2])
-        with col1:
-            scopus_check = st.checkbox('Scopus', True)
-        with col2:
-            num_scopus = st.select_slider('Number of Scopus articles: ', options=[25, 5000], value=5000, disabled=(not scopus_check))
-
-        col1, col2 = st.columns([1,2])
-        with col1:
-            scidir_check = st.checkbox('ScienceDirect', True)
-        with col2:
-            num_scidir = st.select_slider('Number of ScienceDirect articles: ', options=[25, 5000], value=5000, disabled=(not scidir_check))
-
-        @st.cache_data
-        def convert_df(df):
-            return df.to_csv().encode("utf-8")
+        request = basic()
     
     # ADVANCED INPUT
     if mode == "Advanced":
-        # PubMed
-        col1, col2 = st.columns([1,2])
-        with col1:
-            st.subheader('PubMed')
-            pubmed_check = st.checkbox('Enabled', False, key='p')
-        with col2:
-            articles_range = [1, 5, 10, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
-            num_pubmed = st.select_slider('Number of articles: ', options=articles_range, value=5000, disabled=(not pubmed_check), key='p_num')
-        
-        st.markdown('***')
-        # Scopus
-        col1, col2 = st.columns([1,2])
-        with col1:
-            st.subheader('Scopus')
-            scopus_check = st.checkbox('Enabled', False, key='sc')
-        with col2:
-            num_scopus = st.select_slider('Number of articles: ', options=[25, 5000], value=5000, disabled=(not scopus_check), key='sc_num')
+        request = advanced()
 
-        st.markdown('***')
-        # ScienceDirect
-        col1, col2 = st.columns([1,2])
-        with col1:
-            st.subheader('ScienceDirect')
-            scidir_check = st.checkbox('Enabled', False, key='sd')
-        with col2:
-            num_scidir = st.select_slider('Number of articles: ', options=[25, 5000], value=5000, disabled=(not scidir_check), key='sd_num')
-        
+    @st.cache_data
+    def convert_df(df):
+        return df.to_csv().encode("utf-8")
 
 # RESULTADOS
     if st.button("Submit"):
         st.sidebar.write("""## Extracting...""")
 
         # BUSCA PARA O PUBMED
-        if pubmed_check:
-            with st.spinner(f'Searching articles with keyword "{keyword}" in PubMed ({num_pubmed}) wait...'):
-                data_tmp = Extractor(keyword, num_pubmed).pubmed()
+        if request['pm_check']:
+            with st.spinner(f'Searching articles with keyword "{request["pm_keyword"]}" in PubMed ({request["pm_num"]} articles) wait...'):
+                data_tmp = Extractor(request["pm_keyword"], request["pm_num"]).pubmed()
 
                 data = convert_df(data_tmp)
 
@@ -99,9 +54,9 @@ def main():
                 st.success("PubMed Done!")
 
         # BUSCA PARA O Scopus
-        if scopus_check:
-            with st.spinner(f'Searching articles with keyword "{keyword}" in Scopus ({num_scopus}) wait...'):
-                data_tmp = Extractor(keyword, num_scopus).scopus()
+        if request["sc_check"]:
+            with st.spinner(f'Searching articles with keyword "{request["sc_keyword"]}" in Scopus ({request["sc_num"]}) wait...'):
+                data_tmp = Extractor(request["sc_keyword"], request["sc_num"]).scopus()
 
                 data = convert_df(data_tmp)
 
@@ -115,9 +70,9 @@ def main():
                 st.success("Scopus Done!")
         
         # BUSCA PARA O Science Direct
-        if scidir_check:
-            with st.spinner(f'Searching articles with keyword "{keyword}" in ScienceDirect ({num_scidir}) wait...'):
-                data_tmp = Extractor(keyword, num_scidir).scidir()
+        if request["sd_check"]:
+            with st.spinner(f'Searching articles with keyword "{request["sd_keyword"]}" in ScienceDirect ({request["sd_num"]}) wait...'):
+                data_tmp = Extractor(request["sd_keyword"], request["sd_num"]).scidir()
 
                 data = convert_df(data_tmp)
 
---
+++ src/search_input.py
@@ -0,0 +1,109 @@
+import streamlit as st
+
+def basic():
+    keyword = st.text_input("Keyword")
+
+    st.write('Select your desired databases')
+    col1, col2 = st.columns([1,2])
+    with col1:
+        pubmed_check = st.checkbox('PubMed', True)
+    with col2:
+        articles_range = [1, 5, 10, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
+        num_pubmed = st.select_slider('Number of PubMed articles: ', options=articles_range, value=5000, disabled=(not pubmed_check))
+
+    col1, col2 = st.columns([1,2])
+    with col1:
+        scopus_check = st.checkbox('Scopus', True)
+    with col2:
+        num_scopus = st.select_slider('Number of Scopus articles: ', options=[25, 5000], value=5000, disabled=(not scopus_check))
+
+    col1, col2 = st.columns([1,2])
+    with col1:
+        scidir_check = st.checkbox('ScienceDirect', True)
+    with col2:
+        num_scidir = st.select_slider('Number of ScienceDirect articles: ', options=[25, 5000], value=5000, disabled=(not scidir_check))
+
+    request = {
+        'pm_keyword': keyword,
+        'sc_keyword': keyword,
+        'sd_keyword': keyword,
+        'pm_check': pubmed_check,
+        'sc_check': scopus_check,
+        'sd_check': scidir_check,
+        'pm_num': num_pubmed,
+        'sc_num': num_scopus,
+        'sd_num': num_scidir
+    }
+    return request
+
+def advanced():
+    # PubMed
+    col1, col2 = st.columns([1,2])
+    with col1:
+        st.subheader('PubMed')
+        pubmed_check = st.checkbox('Enabled', False, key='p')
+    with col2:
+        articles_range = [1, 5, 10, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
+        num_pubmed = st.select_slider('Number of articles: ', options=articles_range, value=5000, disabled=(not pubmed_check), key='pm_num')
+
+    col1, col2, col3 = st.columns([2, 4, 1])
+    with col1:
+        tag = st.selectbox(
+            '',
+            ('All Fields', 'Date', 'Filter',
+             'Author', 'Affiliation' 'Book', 'Journal', 'Volume', 'Pagination', 'Title', 'Title/Abstract', 'Transliterated Title', 'Text Word',
+             'Language', 'MeSH', 'Pharmacological Action', 'Conflict of Interest Statements', 'EC/RN Number', 'Grant Number', 'ISBN', 'Investigator',
+             'Issue', 'Location ID', 'Secondary Source ID', 'Other Term', 'Publication Type', 'Publisher', 'Subject - Personal Name', 'Supplementary Concept',  
+             ), key='pm_tag', disabled=(not pubmed_check), label_visibility="visible"
+             )
+    with col2:
+        term = st.text_input('Search term', key='pm_term', disabled=(not pubmed_check))
+    with col3:
+        boolean = st.selectbox('', ('AND', 'OR', 'NOT'))
+    
+    query = ''
+    if st.button('Add'):
+        if query == '':
+            query = f'({term})'
+        else:
+            if tag ==  'All Fields':
+                query += f' {boolean} ({term})'
+            else:
+                query += f' {boolean} ({term})[{tag}]'
+        
+    with st.container():
+        st.write(query)
+        pm_keyword = query
+    
+
+    # Scopus
+    st.markdown('***')    
+    col1, col2 = st.columns([1,2])
+    with col1:
+        st.subheader('Scopus')
+        scopus_check = st.checkbox('Enabled', False, key='sc')
+    with col2:
+        num_scopus = st.select_slider('Number of articles: ', options=[25, 5000], value=5000, disabled=(not scopus_check), key='sc_num')
+
+    st.markdown('***')
+    # ScienceDirect
+    col1, col2 = st.columns([1,2])
+    with col1:
+        st.subheader('ScienceDirect')
+        scidir_check = st.checkbox('Enabled', False, key='sd')
+    with col2:
+        num_scidir = st.select_slider('Number of articles: ', options=[25, 5000], value=5000, disabled=(not scidir_check), key='sd_num')
+
+
+    request = {
+        'pm_keyword': pm_keyword,
+        #'sc_keyword': sc_keyword,
+        #'sd_keyword': sd_keyword,
+        'pm_check': pubmed_check,
+        'sc_check': scopus_check,
+        'sd_check': scidir_check,
+        'pm_num': num_pubmed,
+        'sc_num': num_scopus,
+        'sd_num': num_scidir
+    }
+    return request
