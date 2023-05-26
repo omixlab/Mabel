a0c60615daf23df23bd2f5e52b1c2a6cb050f17a
Koku02 <gabriellistondemenek@gmail.com>
2023-05-26 13:38:52 -0300

    Scopus advanced search

--- src/search_input.py
+++ src/search_input.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import filters
+import utils.pubmed as pubmed
+import utils.scopus as scopus
 
 def basic():
     keyword = st.text_input("Keyword")
@@ -44,12 +45,17 @@ def advanced():
     def force_update():
         pass
 
-    # Starts variable 'query' as session state
+    # Start variables as session states
     if 'pm_query' not in st.session_state:
         st.session_state.pm_query = None
-
     if 'selected_filters' not in st.session_state:
-            st.session_state.selected_filters = []
+        st.session_state.selected_filters = []
+
+    if 'sc_query' not in st.session_state:
+        st.session_state.sc_query = None
+
+    if 'sd_query' not in st.session_state:
+        st.session_state.sd_query = None
 
 
     col1, col2 = st.columns([1,2])
@@ -81,10 +87,7 @@ def advanced():
             tag = select
 
     with col2:
-        if select != 'Date':
-            # Text input
-            term = st.text_input('Search term', key='pm_term', disabled=(not pubmed_check))
-        else:
+        if select == 'Date':
             # Date input
             col21, col22 = st.columns([1, 1])
             with col21:
@@ -92,6 +95,9 @@ def advanced():
             with col22:
                 date2 = st.text_input('End', help='YYYY/MM/DD', key='date2')
             term = f'"{date1}"[{tag}] : "{date2}"'
+        else:
+            # Text input
+            term = st.text_input('Search term', key='pm_term', disabled=(not pubmed_check))
         
         # Filtros
         with st.expander('Filters', expanded=False):
@@ -100,7 +106,7 @@ def advanced():
             tabs = [tab1, tab2, tab3, tab4, tab5]
             for n in range(len(tabs)):
                 with tabs[n]:
-                    for filter in filters.pubmed_filters[n]:
+                    for filter in pubmed.pubmed_filters[n]:
                         if st.checkbox(filter, disabled=(not pubmed_check)):
                             st.session_state.selected_filters.append(filter)
 
@@ -108,7 +114,7 @@ def advanced():
             if st.button("Apply filters", disabled=(not pubmed_check)):
                 filters_set = set()
                 for key in st.session_state.selected_filters:
-                    filters_set.add(filters.pumbed_dict.get(key))
+                    filters_set.add(pubmed.pumbed_dict.get(key))
 
                 st.session_state.pm_query += ' AND ('
                 for i, filter in enumerate(filters_set):
@@ -123,17 +129,15 @@ def advanced():
         boolean = st.selectbox('Bool', ('AND', 'OR', 'NOT'), key='bool', disabled=(not pubmed_check), label_visibility="hidden")
 
         # Query
-        if st.button('Add', disabled=(not pubmed_check)):
-            if st.session_state.pm_query is None:
-                if tag == 'All Fields':
-                    st.session_state.pm_query = f'({term})'
-                else:
-                    st.session_state.pm_query = f'({term}[{tag}])'
+        if st.button('Add', disabled=(not pubmed_check), key='pm_add'):
+            if st.session_state.pm_query is not None:
+                st.session_state.pm_query += f' {boolean} '
+
+            if tag == 'All Fields':
+                st.session_state.pm_query = f'({term})'
             else:
-                if tag == 'All Fields':
-                    st.session_state.pm_query += f' {boolean} ({term})'
-                else:
-                    st.session_state.pm_query += f' {boolean} ({term}[{tag}])'
+                st.session_state.pm_query = f'({term}[{tag}])'
+            
             
     pm_keyword = st.text_area('Query', st.session_state.pm_query, disabled=(not pubmed_check), key='pm_keyword', 
                               help='This is the query sent for your search. You can type it manually if you prefer.\n\nLeave blank and press Ctrl+Enter to reset query')
@@ -144,7 +148,7 @@ def advanced():
 
 
 
-    # SCOPUS
+#SCOPUS
     st.markdown('***')    
     col1, col2 = st.columns([1,2])
     with col1:
@@ -156,25 +160,66 @@ def advanced():
     col1, col2, col3 = st.columns([2, 4, 1])
     with col1:
         # Tags
-        select = st.selectbox('All', 'Abstract', 'Affiliation', 'Author', 'Article number', 'Chemical', 
+        select = st.selectbox('Tags', scopus.tags, disabled=(not scopus_check))
 
-        if select == 'Affiliation':
-            tag = st.radio('Affiliation', 'Affiliation ID', 'Affiliation city', 'Affiliation country', 'Affiliation organization')
-        elif select == 'Author':
-            tag = st.radio('Author', 'Author ID', 'Author name', 'Author first initial', 'Author last name', 'Collaboration author', 'Author keywords')
-        elif select == 'Chemical':
-            tag = st.radio('Chemical', 'Chemical name', 'CAS registry number')
-        elif select == 'Author':
-            tag = st.radio(
-        
-                
+        if select in scopus.radios:
+            sc_tag = st.radio('Type', scopus.radios[select])
         else:
-            tag = select
+            sc_tag = select
+
+    with col2:
+        # Search terms
+        if sc_tag in scopus.selects:
+            sc_term = st.selectbox('Search term', scopus.selects[sc_tag])
+            sc_term = scopus.selects[sc_tag][sc_term]
+            
+
+        elif sc_tag == 'Date':
+            col21, col22 = st.columns([1, 4])
+            with col21:
+                operator = st.selectbox('Operator', (' > ', ' = ', ' < '), label_visibility="hidden")
+            with col22:
+                year = st.text_input('Year')
+            sc_term = operator + year
+
+        else:
+            sc_term = st.text_input('Search term', key='sc_term', disabled=(not scopus_check))
+
+        # Filter
+        if st.checkbox('Open access', disabled=(not scopus_check)):
+            if st.session_state.sc_query is None:
+                st.session_state.sc_query = 'OPENACCESS(1)'
+            elif 'OPENACCESS(1)' not in st.session_state.sc_query:
+                st.session_state.sc_query += ' AND OPENACCESS(1)'
+    
+    with col3:
+        # Boolean operator
+        boolean = st.selectbox('Bool', ('AND', 'OR', 'NOT'), key='sc_bool', disabled=(not scopus_check), label_visibility="hidden")
+
+        # Query
+        if st.button('Add', disabled=(not scopus_check), key='sc_add'):
+            if st.session_state.sc_query is not None:
+                st.session_state.sc_query += f' {boolean} '
+            
+            if sc_tag == 'Date':
+                st.session_state.sc_query += f'PUBYEAR {sc_term}'
+            elif sc_tag == 'Reference year':
+                st.session_state.sc_query += f'REFPUBYEAR IS {sc_term}'
+
+            else:
+                if st.session_state.sc_query is None:
+                    st.session_state.sc_query = f'{scopus.field[sc_tag]}({sc_term})'
+                else:
+                    st.session_state.sc_query += f'{scopus.field[sc_tag]}({sc_term})'
+
+        
 
 
 
-
-    sc_keyword = st.text_area('Query', disabled=(not scopus_check), key='sc_keyword')
+    sc_keyword = st.text_area('Query', st.session_state.sc_query, disabled=(not scopus_check), key='sc_keyword', 
+                              help='This is the query sent for your search. You can type it manually if you prefer.\n\nLeave blank and press Ctrl+Enter to reset query')
+    if st.session_state.sc_query is not None:
+        st.session_state.sc_query = sc_keyword
 
 
 
@@ -185,11 +230,14 @@ def advanced():
     col1, col2 = st.columns([1,2])
     with col1:
         st.subheader('ScienceDirect')
-        scidir_check = st.checkbox('Enabled', False, key='sd', disabled=True)
+        scidir_check = st.checkbox('Enabled', False, key='sd')
     with col2:
         num_scidir = st.select_slider('Number of articles: ', options=[25, 5000], value=5000, disabled=(not scidir_check), key='sd_num')
 
-    sd_keyword = st.text_area('Query', disabled=(not scidir_check), key='sd_keyword')
+    sd_keyword = st.text_area('Query', st.session_state.sd_query, disabled=(not scidir_check), key='sd_keyword', 
+                              help='This is the query sent for your search. You can type it manually if you prefer.\n\nLeave blank and press Ctrl+Enter to reset query')
+    if st.session_state.sd_query is not None:
+        st.session_state.sd_query = sd_keyword
 
 
 
@@ -199,7 +247,7 @@ def advanced():
     request = {
         'pm_keyword': pm_keyword,
         'sc_keyword': sc_keyword,
-        #'sd_keyword': sd_keyword,
+        'sd_keyword': sd_keyword,
         'pm_check': pubmed_check,
         'sc_check': scopus_check,
         'sd_check': scidir_check,
---
+++ src/utils/pubmed.py
@@ -0,0 +1,36 @@
+# PUBMED
+pubmed_filters = [
+        ["Abstract", "Free full text", "Full text"],
+        ["Books and documents", "Clinical trial", "Meta-Analysis", "Randomized Controlled Trial", "Review", "Systematic Review"],
+        ["Humans", "Other Animals", "Male", "Female"],
+        ["English", "Portuguese", "Spanish"],
+        ["Associated data", "Exclude preprints", "MEDLINE"]
+        ]
+    
+
+pubmed_dict = {
+        "Abstract": 'fha', 
+        "Free full text": 'ffrft', 
+        "Full text": 'fft',
+        "Books and documents": 'pubt.booksdocs', 
+        "Clinical trial": 'pubt.clinicaltrial',
+        "Meta-Analysis": 'pubt.meta-analysis',
+        "Randomized Controlled Trial": 'pubt.randomizedcontrolledtrial',
+        "Review": 'pubt.review',
+        "Systematic Review": 'pubt.systematicreview',
+
+        "Humans": 'hum_ani.humans',
+        "Other Animals": 'hum_ani.animal',
+        "Male": 'sex.male',
+        "Female": 'sex.female',
+
+        "English": 'lang.english',
+        "Portuguese": 'lang.portuguese',
+        "Spanish": 'lang.spanish',
+
+        "Associated data": 'articleattr.data', 
+        "Exclude preprints": 'other.excludepreprints',
+        "MEDLINE": 'other.medline'
+        }
+
+
