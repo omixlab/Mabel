0f8b7835997cbb99da277a1531bfb6d7b36f172a
Koku02 <gabriellistondemenek@gmail.com>
2023-06-28 16:39:01 -0300

    genes com spacy

--- src/utils/unify_dfs.py
+++ src/utils/unify_dfs.py
@@ -118,5 +118,31 @@ def unify(dfs):
 
 
     # Concatenação dos dataframes
-    return pd.concat(formated_dfs)
+    unified_df = pd.concat(formated_dfs)
+    
+
+
+    # SCISPACY para identificar genes na abstract
+    import scispacy
+    import spacy
+
+    nlp = spacy.load("en_ner_bionlp13cg_md")
+
+    genes_column = []
+    for row in unified_df['Abstract']:
+        doc = nlp(row)
+
+        genes = []
+        for entity in doc.ents:
+            if entity.label_ == 'GENE_OR_GENE_PRODUCT':
+                genes.append(entity.text)
+        genes_column.append(', '.join(set(genes)))
+
+    unified_df.insert(10, 'Gene or gene product', genes_column)
+
+
+
+
+
+    return unified_df
 
