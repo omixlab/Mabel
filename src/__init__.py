800873f0ad202e87f0890c8ba4152902c57af686
gratidutra <gratirodrigues.gdr@gmail.com>
2023-06-28 16:22:47 -0300

    fix duplicated

--- src/__init__.py
+++ src/__init__.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bambu.db"
 app.config["SECRET_KEY"] = "57eea008c38612d210670283"
 db.init_app(app)
-bcrypt=Bcrypt(app)
+bcrypt = Bcrypt(app)
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = "login"
 login_manager.login_message = "Please login"
-login_manager.login_category = 'info'
+login_manager.login_category = "info"
 from src import routes
--- src/forms.py
+++ src/forms.py
@@ -8,17 +8,21 @@ class RegisterForm(FlaskForm):
     def validate_username(self, check_user):
         user = Users.query.filter_by(name=check_user.data).first()
         if user:
-            raise ValidationError('User already exists! Register another user name.')
-        
+            raise ValidationError("User already exists! Register another user name.")
+
     def validate_email(self, check_email):
         email = Users.query.filter_by(email=check_email.data).first()
         if email:
-            raise ValidationError('E-mail already exists! Register another user E-mail.')
-        
+            raise ValidationError(
+                "E-mail already exists! Register another user E-mail."
+            )
+
     def validate_password(self, check_password):
         password = Users.query.filter_by(password=check_password.data).first()
         if password:
-            raise ValidationError('Password already exists! Register another user Password.')
+            raise ValidationError(
+                "Password already exists! Register another user Password."
+            )
 
     name = StringField(
         label="Username:", validators=[Length(min=2, max=30), DataRequired()]
@@ -32,7 +36,8 @@ class RegisterForm(FlaskForm):
     )
     submit = SubmitField(label="Submit")
 
+
 class LoginForm(FlaskForm):
     email = StringField(label="E-mail:", validators=[Email(), DataRequired()])
-    password = PasswordField(label="Senha:",  validators=[DataRequired()])
+    password = PasswordField(label="Senha:", validators=[DataRequired()])
     submit = SubmitField(label="Log In")
